207ae2661541cc8298e8bc6693d7855aca6b1d3b
Maxim Bortnikov <43994704+Northstrix@users.noreply.github.com>
2020-02-26 05:56:43 +0000

    Add files via upload

---
+++ RGB_lamp_2_colors_separated_red_first/RGB_lamp_2_colors_separated_red_first.ino
@@ -0,0 +1,60 @@
+//This sketch is for the two-colored lamp, so technically it's not RGB, but still looks good.
+//Written by Maxim Bortnikov
+/https://github.com/Northstrix/WS2812-based_RGB_lamp
+#include <Adafruit_NeoPixel.h>
+#include "TM1637.h"
+#define LED_PIN    9 // WS2812 data pin
+#define LED_COUNT 20 // The number of LEds
+#define CLK 3      // CLK pin of the TM1637 
+#define DIO 2      // DIO pin of the TM1637
+TM1637 tm1637(CLK,DIO);
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+void setup() {
+  strip.begin(); // Initializes the string
+  strip.show(); // Switching the whole strip off
+  tm1637.init();
+  tm1637.set(BRIGHT_TYPICAL); 
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+
+}
+
+void loop() {
+  int d = analogRead(A1); // Reading the variable for the brightness
+  int g = analogRead(A2)/2; // Reading the variable for yhe color
+  int j = analogRead(A2)/2; // That one for the color too
+  int a,b,c,h,i ;
+
+        if ((g-127)*2 > 255) { // Red color
+    a=0;
+}
+  else {
+    
+        if (g > 127) {
+    h=(g-127)*2;
+    a=g-h;
+    
+}
+  else {
+    a=g-h;
+    h=0;
+  }
+  }
+
+          if (j < 256) { // Blue color
+   b=0;
+}
+  else {
+    b=j-256;
+  }
+
+  int e = d/10.2; // Calculates the percentage of brightness
+  int f = d/4.02; // Calculates the brightness itself
+  strip.setBrightness(f); // That one is setting the brightness
+  uint32_t color = strip.Color(a*2, c, b);
+  strip.fill(color, 0, LED_COUNT);
+  void display(int8_t DispData[]);
+  tm1637.display(e); // Sending the percentage of brightness to the TM1637
+  strip.show();  // That one updates the whole strip at once
+
+}
